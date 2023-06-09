372cf0c2ce4b8dabbd583bec36cbbba585c9f427
gaming0d <80798360+gaming0d@users.noreply.github.com>
2023-06-09 17:19:54 +0700

    vehiclelist

--- src/components/Vehicles/VehicleList.js
+++ src/components/Vehicles/VehicleList.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './VehicleList.css'
 
 const VehicleList = () => {
   const [vehicles, setVehicles] = useState([]);
+  // Menu
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
 
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  //
   useEffect(() => {
     const fetchVehicles = async () => {
       const response = await axios.get('http://localhost:3001/vehicles');
@@ -15,7 +22,39 @@ const VehicleList = () => {
 
   return (
     <div className="container">
-      <h2 className="mt-4">Danh Sách Xe Ô Tô</h2>
+      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+        <div className="menu-title">Main</div>
+        <button className="menu-button">Dashboard</button>
+        <div className="menu-title">Table</div>
+        <button className="menu-button">Giấy đăng kiểm</button>
+        <button className="menu-button">Xe ô tô</button>
+        <button className="menu-button">Thống kê</button>
+      </div>
+      <div className="content">
+        <div className="table-header">Giấy đăng kiểm</div>
+        <div className="search">
+          <input type="text" placeholder="Tìm kiếm theo số đăng kiểm" />
+        </div>
+        <table className="data-table">
+          <thead>
+            <tr>
+              <th>Số đăng kiểm</th>
+              <th>Số đăng ký</th>
+              <th>Ngày đăng kiểm</th>
+              <th>Ngày hết hạn</th>
+              <th>Trung tâm</th>
+              <th>Biển số xe</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Dữ liệu của bảng */}
+          </tbody>
+        </table>
+      </div>
+      <button className="toggle-menu-button" onClick={handleToggleMenu}>
+        Toggle Menu
+      </button>
+      {/* <h2 className="mt-4">Danh Sách Xe Ô Tô</h2>
       <Link to="/upload-vehicle" className="btn btn-primary mb-3">
         Tải Lên Xe
       </Link>
@@ -38,7 +77,7 @@ const VehicleList = () => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table> */}
     </div>
   );
 };
