bcc97f01323f0922c248f578ec87893322fc39e7
gaming0d <80798360+gaming0d@users.noreply.github.com>
2023-06-11 19:44:46 +0700

    Update Car.js

--- src/components/Office/Car.js
+++ src/components/Office/Car.js
@@ -5,7 +5,13 @@ import BaseLayout from './Base';
 const Car = ({ customers }) => {
   const [cars, setCars] = useState([]);
   const [editedCar, setEditedCar] = useState(null);
-  const [searchTerms, setSearchTerms] = useState({});
+  const [searchTerms, setSearchTerms] = useState([]);
+  const [searchAttributes, setSearchAttributes] = useState([
+    {
+      attribute: 'registration_number',
+      value: ''
+    }
+  ]);
 
   useEffect(() => {
     fetch('http://localhost:8000/cars/')
@@ -60,20 +66,80 @@ const Car = ({ customers }) => {
     }
   };
 
-  const handleSearchChange = (e, attribute) => {
-    const value = e.target.value;
-    setSearchTerms((prevSearchTerms) => ({ ...prevSearchTerms, [attribute]: value }));
+  const handleSearchAttributeChange = (e, index) => {
+    const { value } = e.target;
+    setSearchAttributes((prevSearchAttributes) => {
+      const updatedSearchAttributes = [...prevSearchAttributes];
+      updatedSearchAttributes[index] = { ...updatedSearchAttributes[index], attribute: value };
+      return updatedSearchAttributes;
+    });
   };
 
-  const filteredCars = cars.filter((car) => {
-    for (const attribute in searchTerms) {
-      const searchTerm = searchTerms[attribute];
-      if (searchTerm && !car[attribute].toLowerCase().includes(searchTerm.toLowerCase())) {
-        return false;
-      }
-    }
-    return true;
-  });
+  const handleSearchValueChange = (e, index) => {
+    const { value } = e.target;
+    setSearchAttributes((prevSearchAttributes) => {
+      const updatedSearchAttributes = [...prevSearchAttributes];
+      updatedSearchAttributes[index] = { ...updatedSearchAttributes[index], value: value };
+      return updatedSearchAttributes;
+    });
+  };
+
+  const handleAddSearchAttribute = () => {
+    setSearchAttributes((prevSearchAttributes) => {
+      const remainingAttributes = searchAttributes.filter((attribute) => attribute.value !== '');
+      return [
+        ...remainingAttributes,
+        {
+          attribute: '',
+          value: ''
+        }
+      ];
+    });
+  };
+
+  const handleRemoveSearchAttribute = (index) => {
+    setSearchAttributes((prevSearchAttributes) =>
+      prevSearchAttributes.filter((_, i) => i !== index)
+    );
+  };
+
+  const handleSearch = () => {
+    const filteredCars = cars.filter((car) =>
+      searchAttributes.every(
+        (attribute) =>
+          car[attribute.attribute].toLowerCase().includes(attribute.value.toLowerCase())
+      )
+    );
+    setSearchTerms(filteredCars);
+  };
+
+  const availableAttributes = [
+    'Registration Number',
+    'Registration Date',
+    'License Plate',
+    'Manufacturer',
+    'Model',
+    'Version',
+    'Engine Capacity',
+    'Power',
+    'Torque',
+    'Transmission',
+    'Seating Capacity',
+    'Length',
+    'Width',
+    'Height',
+    'Weight',
+    'Fuel Consumption',
+    'Suspension',
+    'Braking System',
+    'Purpose',
+    'Owner'
+  ];
+
+  const getAvailableAttributes = () => {
+    const selectedAttributes = searchAttributes.map((attribute) => attribute.attribute);
+    return availableAttributes.filter((attribute) => !selectedAttributes.includes(attribute));
+  };
 
   return (
     <BaseLayout>
@@ -81,6 +147,42 @@ const Car = ({ customers }) => {
         <div className="panel panel-primary">
           <div className="panel-heading">
             <h6 className="panel-title">Cars</h6>
+          </div>
+          <div className="search-container">
+            {searchAttributes.map((attribute, index) => (
+              <div key={index} className="search-input-container">
+                <select
+                  value={attribute.attribute}
+                  onChange={(e) => handleSearchAttributeChange(e, index)}
+                >
+                  {availableAttributes.map((availableAttribute) => (
+                    <option key={availableAttribute} value={availableAttribute}>
+                      {availableAttribute}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="text"
+                  value={attribute.value}
+                  onChange={(e) => handleSearchValueChange(e, index)}
+                  placeholder="Search..."
+                />
+                {index > 0 && (
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleRemoveSearchAttribute(index)}
+                  >
+                    Remove
+                  </button>
+                )}
+              </div>
+            ))}
+            <button className="btn btn-primary" onClick={handleAddSearchAttribute}>
+              Add Attribute
+            </button>
+            <button className="btn btn-primary" onClick={handleSearch}>
+              Search
+            </button>
           </div>
           <table className="table table-hover" id="dev-table">
             <thead>
@@ -108,208 +210,63 @@ const Car = ({ customers }) => {
                 <th>Owner</th>
                 <th>Actions</th>
               </tr>
-              <tr>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Registration Number"
-                    onChange={(e) => handleSearchChange(e, 'registration_number')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Registration Date"
-                    onChange={(e) => handleSearchChange(e, 'registration_date')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search License Plate"
-                    onChange={(e) => handleSearchChange(e, 'license_plate')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Registered Location"
-                    onChange={(e) => handleSearchChange(e, 'registered_location')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Manufacturer"
-                    onChange={(e) => handleSearchChange(e, 'manufacturer')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Model"
-                    onChange={(e) => handleSearchChange(e, 'model')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Version"
-                    onChange={(e) => handleSearchChange(e, 'version')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Engine Capacity"
-                    onChange={(e) => handleSearchChange(e, 'engine_capacity')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Power"
-                    onChange={(e) => handleSearchChange(e, 'power')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Torque"
-                    onChange={(e) => handleSearchChange(e, 'torque')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Transmission"
-                    onChange={(e) => handleSearchChange(e, 'transmission')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Seating Capacity"
-                    onChange={(e) => handleSearchChange(e, 'seating_capacity')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Length"
-                    onChange={(e) => handleSearchChange(e, 'length')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Width"
-                    onChange={(e) => handleSearchChange(e, 'width')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Height"
-                    onChange={(e) => handleSearchChange(e, 'height')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Weight"
-                    onChange={(e) => handleSearchChange(e, 'weight')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Fuel Consumption"
-                    onChange={(e) => handleSearchChange(e, 'fuel_consumption')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Suspension"
-                    onChange={(e) => handleSearchChange(e, 'suspension')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Braking System"
-                    onChange={(e) => handleSearchChange(e, 'braking_system')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Purpose"
-                    onChange={(e) => handleSearchChange(e, 'purpose')}
-                  />
-                </th>
-                <th>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Owner"
-                    onChange={(e) => handleSearchChange(e, 'owner')}
-                  />
-                </th>
-                <th></th>
-              </tr>
             </thead>
             <tbody>
-              {filteredCars.map((car, index) => (
+              {searchTerms.map((car, index) => (
                 <tr key={car.registration_number}>
-                  {Object.entries(car).map(([key, value]) => (
-                    <td key={key}>
-                      {editedCar === car ? (
-                        <input
-                          type="text"
-                          name={key}
-                          value={value}
-                          onChange={(e) => handleInputChange(e, index)}
-                        />
-                      ) : (
-                        value
-                      )}
-                    </td>
-                  ))}
+                  <td>{car.registration_number}</td>
+                  <td>{car.registration_date}</td>
+                  <td>{car.license_plate}</td>
+                  <td>{car.registered_location}</td>
+                  <td>{car.manufacturer}</td>
+                  <td>{car.model}</td>
+                  <td>{car.version}</td>
+                  <td>{car.engine_capacity}</td>
+                  <td>{car.power}</td>
+                  <td>{car.torque}</td>
+                  <td>{car.transmission}</td>
+                  <td>{car.seating_capacity}</td>
+                  <td>{car.length}</td>
+                  <td>{car.width}</td>
+                  <td>{car.height}</td>
+                  <td>{car.weight}</td>
+                  <td>{car.fuel_consumption}</td>
+                  <td>{car.suspension}</td>
+                  <td>{car.braking_system}</td>
+                  <td>{car.purpose}</td>
+                  <td>{car.owner}</td>
                   <td>
-                    {editedCar === car ? (
-                      <button className="btn btn-primary" onClick={() => handleSave(car.registration_number)}>
-                        Save
-                      </button>
+                    {editedCar && editedCar.registration_number === car.registration_number ? (
+                      <div>
+                        <button
+                          className="btn btn-success"
+                          onClick={() => handleSave(car.registration_number)}
+                        >
+                          Save
+                        </button>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => setEditedCar(null)}
+                        >
+                          Cancel
+                        </button>
+                      </div>
                     ) : (
-                      <button className="btn btn-primary" onClick={() => handleEdit(car)}>
-                        Edit
-                      </button>
+                      <div>
+                        <button
+                          className="btn btn-warning"
+                          onClick={() => handleEdit(car)}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => handleDelete(car.registration_number)}
+                        >
+                          Delete
+                        </button>
+                      </div>
                     )}
-                    <button className="btn btn-danger" onClick={() => handleDelete(car.registration_number)}>
-                      Delete
-                    </button>
                   </td>
                 </tr>
               ))}
