434dd70cbf3c973a914c9839afa3a9657510b87f
gaming0d <80798360+gaming0d@users.noreply.github.com>
2023-06-12 22:51:24 +0700

    dashboard

--- src/components/Office/Dashboard.js
+++ src/components/Office/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import BaseLayout from './Base';
-
 import {
   Tooltip,
   XAxis,
